d6e223762632d9b43970d929cc98bed4ad15a0e3
katrinLuna <kate.nik2017@yandex.ru>
2020-01-28 10:53:14 +0300

    generate content for signgle item and add routing back

--- build/description-generate.js
+++ build/description-generate.js
@@ -1,9 +1,27 @@
 'use strict';
 
 (function () {
+    let itemInfoElment = document.querySelector('.item-info__text');
     
     let generateInfo = function (data) {
+        itemInfoElment.querySelector('.item-info__title').textContent = data.title;
+        itemInfoElment.querySelector('.item-info__address').textContent = data.address;
+        itemInfoElment.querySelector('.item-info__price').textContent = data.price;
+        itemInfoElment.querySelector('.item-info__description').textContent = data.description;
+        itemInfoElment.querySelector('.item-info__sellerName').textContent = data.sellerName;
 
     };
+
+    window.descriptionGenerate = generateInfo;
+
+    // <!-- [{"id":1849621339,
+    // "title":"2-к квартира, 85.5 м², 13/24 эт.",
+    // "address":"Спартаковская ул., 5/7",
+    // "price":"14 150 000 руб.",
+    // "description":"Продается 2 комнатная квартира, Общая площадь 85,5 кв.м . Квартира расположена на 13 этаже 22 этажного моно-кирпичного дома, построенного в 2002 году. 2 изолированные комнаты (20 и 15 кв.м.), кухня –столовая 16 кв.м., отличное состояние, полностью готова для проживания (оставляем всю мебель и технику), сделан косметический ремонт, полы в комнатах паркет, кухня – кафель, коридор кафель и ламинат, 2 кондиционера.",
+    // "sellerName":"Анна",
+
+    // "images":["http://134.209.138.34/images/large/6329074398.jpg","http://134.209.138.34/images/large/6329074586.jpg","http://134.209.138.34/images/large/6329075095.jpg","http://134.209.138.34/images/large/6329075174.jpg"]}] -->
+
 
 } ());
--- build/routing.js
+++ build/routing.js
@@ -4,16 +4,23 @@
     const DATA_URL = 'http://134.209.138.34/item/';
     const ESTATES_LIST_ELEMENT = document.querySelector('.realestate-list');
     const ESTATE_ITEM_PAGE = document.querySelector('.item-info-wrapper');
+    const BACK_BTN = document.querySelector('.item-info__back-btn');
+
     let routing = {
-        home: 1,
         page: async function (id) {
             ESTATES_LIST_ELEMENT.classList.toggle('visually-hidden');
             let data = await window.downloadData(DATA_URL + id);
-            console.log(data);
-            // вызов функции генерации страницы
+            console.log(data[0]);
+            window.descriptionGenerate(data[0]);
+            // вызов функции генерации страницы data[0]
             ESTATE_ITEM_PAGE.classList.toggle('visually-hidden');
         }
     }
+
+    BACK_BTN.addEventListener('click', function () {
+        ESTATES_LIST_ELEMENT.classList.toggle('visually-hidden');
+        ESTATE_ITEM_PAGE.classList.toggle('visually-hidden');
+    });
 
     window.routing = routing;
 
